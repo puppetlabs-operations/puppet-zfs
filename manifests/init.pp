c3e1e4ae06aa1126d312d530f3e5b92897b31a89
Ben Hughes <ben@puppetlabs.com>
2012-05-01 16:42:24 -0700

    Give Solaris a /usr/local, install things to it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,15 +7,13 @@ class zfs {
       package{ 'sysutils/zfs-stats': ensure => present, }
     }
     'Solaris': {
-      # Yes, I am installing them in /usr/sbin/ because Solaris doesn't
-      # have a /usr/local by default, nor does it's $PATH for things.
       file{
-        '/usr/sbin/arc_summary.pl':
+        '/usr/local/sbin/arc_summary.pl':
           ensure => present,
           mode   => '0755',
           owner  => 'root',
           source => 'puppet:///modules/zfs/arc_summary.pl';
-        '/usr/sbin/arcstat.pl':
+        '/usr/local/sbin/arcstat.pl':
           ensure => present,
           mode   => '0755',
           owner  => 'root',
