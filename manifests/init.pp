5ffa957f91a23b286f2fc83a9b2fc90d8d034c90
Ben Hughes <ben@puppetlabs.com>
2012-05-01 16:35:58 -0700

    Install the ZFS arc scripts on Solaris.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,27 @@ class zfs {
 
   include zfs::scrubber
 
-  package{ 'sysutils/zfs-stats': ensure => present, }
+  case $::operatingsystem {
+    'FreeBSD': {
+      package{ 'sysutils/zfs-stats': ensure => present, }
+    }
+    'Solaris': {
+      # Yes, I am installing them in /usr/sbin/ because Solaris doesn't
+      # have a /usr/local by default, nor does it's $PATH for things.
+      file{
+        '/usr/sbin/arc_summary.pl':
+          ensure => present,
+          mode   => '0755',
+          owner  => 'root',
+          source => 'puppet:///modules/zfs/arc_summary.pl';
+        '/usr/sbin/arcstat.pl':
+          ensure => present,
+          mode   => '0755',
+          owner  => 'root',
+          source => 'puppet:///modules/zfs/arcstat.pl',
+      }
+    }
+    default: { fail( "Sorry, ZFS isn't done for ${::operatingsystem}." ) }
+  }
 
 }
