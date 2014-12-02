1dcf48cf0d2a9542f56bf7e95a95c6a448ef02fe
kirby@puppetlabs.com <kirby@puppetlabs.com>
2014-12-02 15:42:55 -0800

    Adding a more flexible share class that lets you declare the whole statement yourself, or just parts of it, which should be more user friendly

--- manifests/share.pp
+++ manifests/share.pp
@@ -1,24 +1,95 @@
 # A class for managing ZFS shares
 define zfs::share (
-  $share,
-  $parent    = undef,
-  $path      = '/usr/bin:/usr/sbin',
+# $share,
+# $parent      = undef,
+  $allow_ip    = undef,
+  $full_share  = undef,
+  $share_title = undef,
+  $protocol    = 'nfs',
+  $security    = [ 'sys', 'default', 'none' ],
+  $sec_none    = 'none',
+  $sec_sys     = 'sys',
+  $sec_default = 'default',
+  $permissions = 'rw',
+  $zpool       = undef,
+  $zvol        = $title,
+  $path        = '/usr/bin:/usr/sbin',
 ) {
 
-  if $parent {
-    $vol_name = "${parent}/${title}"
+  include zfs::vol::get_share
+
+  if $zpool {
+    $vol_name = "${zpool}/${zvol}"
   }
   else {
-    $vol_name = $title
+    $vol_name = $zvol
+  }
+
+  if $share_title {
+    $share_name = $share_title
+  }
+  else {
+    $share_name = "${zpool}_${zvol}"
+  }
+
+  if ( is_array($allow_ip) ) {
+    $addresses = inline_template("<%= allow_ip.join(':@') %>")
+  }
+  else {
+    $addresses = $allow_ip
+  }
+
+  # Build commands
+  $share_base   = "name=${share_name},path=/${vol_name}"
+  $share_prot   = "prot=${protocol}"
+  $share_sec    = "sec=${security}"
+  $share_perm   = "${permissions}=@${addresses}"
+  $base_command = "${share_base},${share_prot}"
+
+  if ( is_array($security) ) {
+    case $security {
+      /(?=(.*sys))(?=(.*default))/: {
+        $share_command = "${base_command},sec=sys,${share_perm},sec=default,${share_perm}"
+      }
+      /(?=(.*sys))(?=(.*none))/: {
+        $share_command = "${base_command},sec=sys,${share_perm},sec=none,${share_perm}"
+      }
+      /(?=(.*default))(?=(.*none))/: {
+        $share_command = "${base_command},sec=default,${share_perm},sec=none,${share_perm}"
+      }
+      /(?=(.*sys))(?=(.*default))(?=(.*none))/: {
+        $share_command = "${base_command},sec=sys,${share_perm},sec=default,${share_perm},sec=none,${share_perm}"
+      }
+    }
+  }
+  else {
+    $share_command = "${base_command},${share_sec},${share_perm}"
+  }
+
+  if ( $full_share == undef ) {
+    $share = $share_command
+  }
+  else {
+    $share = $full_share
   }
 
   $set_zfs_share = "zfs set share=\"${share}\" ${vol_name}"
 
-  include zfs::vol::get_share
+  if ! defined(Zfs[$vol_name]) {
+    zfs { $vol_name:
+      sharenfs => 'on',
+    }
+  }
 
   exec { $set_zfs_share:
     unless  => "zfs_get_share ${vol_name} ${share}",
     path    => $path,
     require => [ Zfs[$vol_name], Class[zfs::vol::get_share] ]
+  }
+
+  ::zfs::share { $vol_name:
+    parent  => $zpool,
+    share   => $share,
+    require => Zfs[$vol_name],
   }
 }
