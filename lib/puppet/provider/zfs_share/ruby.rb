40ff9660360be5872e1e2a2ad477f07e3f9bc1ba
kirby@puppetlabs.com <kirby@puppetlabs.com>
2015-05-05 00:02:08 -0700

    Try another method for referencing source data

--- lib/puppet/provider/zfs_share/ruby.rb
+++ lib/puppet/provider/zfs_share/ruby.rb
@@ -4,10 +4,9 @@ Puppet::Type.type(:zfs_share).provide(:ruby) do
   desc "ZFS share support for Solaris 11"
 
   commands :zfscmd => "zfs"
-  share_title = @resource[:name].gsub('/', '_')
-  share_path = @resource[:name].insert 0, "/"
-  allow_ip = @resource[:allow_ip]
-  allow_ips = @allow_ip.insert(0, '@')
+  share_title = resource.value(:name).gsub('/', '_')
+  share_path = resource.value(:name).insert 0, "/"
+  allow_ip = resource.value(:allow_ip).insert(0, '@')
   # REPLACE ME
   security = 'none'
   permission = 'rw'
