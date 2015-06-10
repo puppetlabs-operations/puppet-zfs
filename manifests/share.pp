42c1fe1869b69d6fd5b4514e0018458ffb41749d
kirby@puppetlabs.com <mk.kirby@gmail.com>
2015-06-10 12:10:17 -0700

    Update zfs share to support read and write ip
    This commit update zfs share to support setting a read ip default value,
    read, and write IP addresses separately. Without this change it is not
    possible to grant access to a default read ip, along with separate read
    and write IP addresses. Additionally, nosub and anon options are added
    to the share define.

--- manifests/share.pp
+++ manifests/share.pp
@@ -1,24 +1,22 @@
 # A class for managing ZFS shares
 define zfs::share (
-  $ensure      = present,
-  $destructive = false,
-  $zvol        = $title,
-  $protocol    = 'nfs',
-  $permissions = 'rw',
-  $security    = [ 'sys', 'default', 'none' ],
-  $path        = '/usr/bin:/usr/sbin',
-  $nosub       = 'on',
-  $allow_ip    = undef,
-  $zpool       = undef,
-  $full_share  = undef,
-  $share_title = undef
+  $ensure                = present,
+  $destructive           = false,
+  $zvol                  = $title,
+  $protocol              = 'nfs',
+  $security              = [ 'sys', 'default', 'none' ],
+  $path                  = '/usr/bin:/usr/sbin',
+  $allow_ip_read         = undef,
+  $allow_ip_read_default = undef,
+  $allow_ip_write        = undef,
+  $zpool                 = undef,
+  $full_share            = undef,
+  $share_title           = undef,
+  $anon_user_id          = undef,
+  $nosub                 = undef
 ) {
 
   include zfs::vol::get_share
-
-  if ( is_array($permissions) ) {
-    fail( 'An array of permissions is not supported' )
-  }
 
   if $zpool {
     $vol_name = "${zpool}/${zvol}"
@@ -32,27 +30,53 @@ define zfs::share (
     $share_name = regsubst($vol_name, '/', '_', 'G')
   }
 
-  if $allow_ip {
-    if ( is_array($allow_ip) ) {
-      $addresses = inline_template("@<%= allow_ip.join(':@') %>")
+  if $allow_ip_read {
+    if is_array($allow_ip_read) {
+      $allow_ips_read_j = join($allow_ip_read, ':@')
+      $allow_ips_read   = "@${allow_ips_read_j}"
+    } else {
+      $allow_ips_read = [ "@${allow_ip_read}" ]
     }
-    else {
-      case $allow_ip {
-        /(^\*$)/: {
-          $addresses = '*'
-        }
-        default: {
-          $addresses = "@${allow_ip}"
-        }
+  } else {
+    $allow_ips_read = []
+  }
+
+  if $allow_ip_read_default {
+    if is_array($allow_ip_read_default) {
+      $allow_ips_read_default_j = join($allow_ip_read_default, ':@')
+      $allow_ips_read_default   = "@${allow_ips_read_default_j}"
+    } else {
+      $allow_ips_read_default = "@${allow_ip_read_default}"
+    }
+  } else {
+    $allow_ips_read_default = []
+  }
+
+  $allow_read_ips = concat($allow_ips_read, $allow_ips_read_default)
+
+  if ! empty($allow_read_ips) {
+    $allow_read_cmd = "ro=${allow_read_ips}"
+  }
+
+  if $allow_ip_write {
+    if is_array($allow_ip_write) {
+      $allow_ips_write_j = join($allow_ip_write, ':@')
+      $allow_ips_write   = "@${allow_ips_write}"
+    } else {
+      if $allow_ip_write =~ /(^\*$)/ {
+        $allow_ips_write = $allow_ip_write
+      } else {
+        $allow_ips_write = "@${allow_ip_write}"
       }
     }
+    $allow_write_cmd = "rw=${allow_ips_write}"
   }
 
   if ( is_array($protocol) ) {
     case $protocol {
       /(?=(.*nfs))(?=(.*smb))/: {
         $share_prot     = 'prot=nfs'
-        $share_prot_smb = ",prot=smb,guestok=true,${permissions}=*"
+        $share_prot_smb = ',prot=smb,guestok=true,rw=*'
       }
       default: {
         fail( '$protocol array is invalid' )
@@ -63,12 +87,20 @@ define zfs::share (
     $share_prot = "prot=${protocol}"
   }
 
+  if $anon_user_id {
+    $anon_user = "anon=${anon_user_id}"
+  }
+
+  if $nosub {
+    $nosub_c = 'nosub=true'
+  }
+
   # Build commands
   $set_share    = 'zfs set share'
   $unset_share  = 'zfs set -c share'
   $share_base   = "name=${share_name},path=/${vol_name}"
   $share_sec    = "sec=${security}"
-  $share_perm   = "${permissions}=${addresses}"
+  $share_perm   = "rw=${allow_ips_write}"
   $base_command = "${share_base},${share_prot}"
   $sec_sys      = "sec=sys,${share_perm}"
   $sec_default  = "sec=default,${share_perm}"
@@ -94,7 +126,18 @@ define zfs::share (
     }
   }
   else {
-    $share_command = "${base_command},${share_sec},${share_perm}${share_prot_smb}"
+    $share_base_a = [
+      $share_base,
+      $share_prot,
+      $anon_user,
+      $nosub_c,
+      $share_sec,
+      $allow_read_cmd,
+      $allow_write_cmd,
+      $share_prot_smb
+    ]
+    $share_remove_e = delete_undef_values($share_base_a)
+    $share_command  = join($share_remove_e, ',')
   }
 
   if $full_share {
@@ -125,9 +168,6 @@ define zfs::share (
         }
       } else {
         exec { $set_zfs_share: }
-        if $nosub {
-          exec { "zfs set share.nfs.nosub=on ${vol_name}": }
-        }
       }
     }
   }
